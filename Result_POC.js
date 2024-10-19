6d550aa6357d51588278f3246c1fda9a8dd499f7
Vedant Bakshi <vedantbakshi07@gmail.com>
2024-10-19 23:42:17 +0530

    Create Result_POC.js

---
+++ Result_POC.js
@@ -0,0 +1,21 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'SCRIPT_RESULT') {
+        const result = message.result;
+        chrome.windows.create({
+            url: chrome.runtime.getURL('results.html'),
+            type: 'popup',
+            width: 600,
+            height: 400
+        }, function(window) {
+            const newTabId = window.tabs[0].id;
+
+            chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+                if (tabId === newTabId && info.status === 'complete') {
+                    console.log('Sending message to the results window'); 
+                    chrome.tabs.sendMessage(newTabId, { type: 'DISPLAY_RESULT', result: result });
+                    chrome.tabs.onUpdated.removeListener(listener);
+                }
+            });
+        });
+    }
+});
